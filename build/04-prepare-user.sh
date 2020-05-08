8e84992d3cf5aea53faa9e431d328575f9e63003
Freek Gruntjes <freek@hipex.io>
2020-05-08 19:54:22 +0200

    PHP user config fix

--- build/04-prepare-user.sh
+++ build/04-prepare-user.sh
@@ -3,11 +3,20 @@
 set -e
 
 mkdir /app
-addgroup --system app
-adduser \
-    --system \
-    --home /app \
-    --shell /bin/shell \
-    app app
+if [[ "$PHP_VERSION" =~ ^7.0 ]]; then
+    addgroup -S app
+    adduser \
+        -S \
+        -h /app \
+        -s /bin/shell \
+        app app
+else
+    addgroup --system app
+    adduser \
+        --system \
+        --home /app \
+        --shell /bin/shell \
+        app app
+fi
 
 chown app:app /app
